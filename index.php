93eed45cf38e2d455f49ac9bbfe2069a2d016112
Reza <reza55n@gmail.com>
2021-10-28 20:18:26 +0330

    Update index.php
    Reconnect after closing SSE connection

--- index.php
+++ index.php
@@ -16,10 +16,65 @@
 	<div id="result"></div>
 
 	<script>
-		var source = new EventSource("getMessages.php");
-		source.onmessage = function(event) {
-			document.getElementById("result").innerHTML += event.data + "<br>";
-		};
+		
+		// Begin codes from https://stackoverflow.com/a/54385402/5371561
+		function isFunction(functionToCheck) {
+			return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
+		}
+
+		function debounce(func, wait) {
+			var timeout;
+			var waitFunc;
+			
+			return function() {
+				if (isFunction(wait)) {
+					waitFunc = wait;
+				}
+				else {
+					waitFunc = function() { return wait };
+				}
+				
+				var context = this, args = arguments;
+				var later = function() {
+					timeout = null;
+					func.apply(context, args);
+				};
+				clearTimeout(timeout);
+				timeout = setTimeout(later, waitFunc());
+			};
+		}
+
+		// reconnectFrequencySeconds doubles every retry
+		var reconnectFrequencySeconds = 1;
+		var evtSource;
+
+		var reconnectFunc = debounce(function() {
+			setupEventSource();
+			// Double every attempt to avoid overwhelming server
+			reconnectFrequencySeconds *= 2;
+			// Max out at ~1 minute as a compromise between user experience and server load
+			if (reconnectFrequencySeconds >= 64) {
+				reconnectFrequencySeconds = 64;
+			}
+		}, function() { return reconnectFrequencySeconds * 1000 });
+
+		function setupEventSource() {
+			evtSource = new EventSource("getMessages.php"); 
+			evtSource.onmessage = function(e) {
+				document.getElementById("result").innerHTML += e.data + "<br>";
+			};
+			evtSource.onopen = function(e) {
+			// Reset reconnect frequency upon successful connection
+			reconnectFrequencySeconds = 1;
+			};
+			evtSource.onerror = function(e) {
+			evtSource.close();
+			reconnectFunc();
+			};
+		}
+		setupEventSource();
+		// End codes from https://stackoverflow.com/a/54385402/5371561
+		
 		
 		function sendMsg()
 		{
@@ -35,4 +90,4 @@
 	</script>
 
 </body>
-</html> 
+</html>
